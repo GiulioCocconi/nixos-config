5186bea1016a9f4acd9d80f9c517a0279d002215
Giulio Cocconi <coccogiulio8@gmail.com>
2023-08-03 14:33:34 +0200

    Added light module

---
+++ modules/light/default.nix
@@ -0,0 +1,13 @@
+{lib, config, options, ...}:
+with lib;
+
+let
+  cfg = config.cogisys.light;
+in
+{
+  options.cogisys.light = {
+    enable = mkEnableOption "light system";
+    memory = mkBoolOpt cfg.enable "Set to true if system has little memory";
+    storage = mkBoolOpt cfg.enable "Set to true if system has little storage space";
+  };
+}
