548eaaa5f5e308f298ea8c43c195e4a9f4e80065
Giulio Cocconi <coccogiulio8@gmail.com>
2023-08-13 17:54:40 +0000

    Done

--- modules/system/boot/default.nix
+++ modules/system/boot/default.nix
@@ -5,24 +5,34 @@ with lib;
 let
   cfg = config.cogisys.system.boot;
 in
-  {
-    options.cogisys.system.boot = with types; {
-      enable = mkEnableOption "booting.";
-      dualBoot = mkBoolOpt false "Is the system dual-booting?"; # If true enable osprober
-      rootFilesystem = mkOpt (enum ["ext4" "zfs" "btrfs"]) "ext4" "Filesystem of root";
-    };
+{
+  options.cogisys.system.boot = with types; {
+    enable = mkEnableOption "booting.";
+    dualBoot = mkBoolOpt false "Is the system dual-booting?";
+    rootFilesystem = mkOpt (enum ["ext4" "zfs" "btrfs"]) "ext4" "Filesystem of root";
+    mode = mkOpt (enum ["legacy" "UEFI"]) "UEFI" "Boot Mode";
+    device = mkOpt types.str "nodev" "Where to install the bootloader";
+  };
 
-    config = mkMerge [
-      (mkIf cfg.enable {
-        boot = {
-          loader = {
-          # TODO!
-        };
-        supportedFilesystems = [ cfg.rootFilesystem ];
-      };
-    })
-    (mkIf (cfg.enable && cfg.dualBoot) {
-        #TODO: Enable osprober
-      })
+  config = mkIf cfg.enable {
+    assertions = [
+      {
+        assertion = (cfg.mode == "legacy") -> (cfg.device != "nodev");
+        message = "If you're booting in legacy mode you should specify a boot device!";
+      }
+      {
+        assertion = (cfg.mode == "UEFI") -> (cfg.device == "nodev");
+        message = "If you're booting in UEFI mode you should use \"nodev\" as boot device!";
+      }
     ];
-  }
+    boot = {
+      loader = {
+        grub.enabled = (cfg.mode == "legacy");
+        systemd-boot.enabled = (cfg.mode == "UEFI");
+        grub.device = cfg.device;
+        grub.useOSProber = cfg.dualBoot;
+      };
+      supportedFilesystems = [ cfg.rootFilesystem ];
+    };
+  };
+}
