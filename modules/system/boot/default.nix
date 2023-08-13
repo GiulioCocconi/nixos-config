5bc1a3b7eccd81fe406915235a33d5389f69c1db
Giulio Cocconi <coccogiulio8@gmail.com>
2023-08-13 15:39:21 +0000

    fixed

--- modules/system/boot/default.nix
+++ modules/system/boot/default.nix
@@ -7,14 +7,9 @@ let
 in
   {
     options.cogisys.system.boot = with types; {
-<<<<<<< HEAD
       enable = mkEnableOption "booting.";
       dualBoot = mkBoolOpt false "Is the system dual-booting?"; # If true enable osprober
       rootFilesystem = mkOpt (enum ["ext4" "zfs" "btrfs"]) "ext4";
-=======
-      enable = mkBoolOpt false "Enable booting.";
-      dualBoot = mkBoolOpt false "Is the system dual-booting?";
->>>>>>> parent of 2a5cb23 (Using mkEnableOption in all modules)
     };
 
     config = mkMerge [
