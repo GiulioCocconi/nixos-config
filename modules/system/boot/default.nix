c24bf312868687b1fe18226ae440d873c30650d1
Giulio Cocconi <coccogiulio8@gmail.com>
2023-08-13 15:34:35 +0000

    Enable zsh support

--- modules/system/boot/default.nix
+++ modules/system/boot/default.nix
@@ -9,11 +9,20 @@ in
     options.cogisys.system.boot = with types; {
       enable = mkEnableOption "booting.";
       dualBoot = mkBoolOpt false "Is the system dual-booting?"; # If true enable osprober
+      rootFilesystem = mkOpt (enum ["ext4" "zfs" "btrfs"]) "ext4";
     };
 
-    config = mkIf cfg.enable {
-      boot.loader = {
-      # TODO!
-    };
-  };
-}
+    config = mkMerge [
+      (mkIf cfg.enable {
+        boot = {
+          loader = {
+          # TODO!
+        };
+      };
+      supportedFilesystems = [ cfg.rootFilesystem ];
+    })
+    (mkIf (cfg.enable && cfg.dualBoot) {
+        #TODO: Enable osprober
+      })
+    ];
+  }
--- modules/system/networking/default.nix
+++ modules/system/networking/default.nix
@@ -3,30 +3,35 @@ with lib;
 
 let
   cfg = config.cogisys.system.networking;
+  rootFS = config.cogisys.system.boot.rootFilesystem;
 in
   {
     options.cogisys.system.networking = with types; {
       enable = mkEnableOption "networking";
-      wifi = { enable = mkEnableOption "WiFi"; };
+      wifi = { enable = mkEnableOption "wifi" };
     };
-
     config = mkMerge [
       (mkIf cfg.enable {
-        networking.networkmanager.enable = true;
-        networking.firewall.enable = true;
+        networking = {
+          networkmanager.enable = true;
+          firewall.enable = true;
+          hostId = if (rootFS == "zfs") then
+            builtins.substring 0 8 (builtins.readFile /etc/machine-id)
+            else null;
+        };
 
 
-      # Fixes issue https://github.com/NixOS/nixpkgs/issues/195777
-      system.activationScripts = with pkgs; {
-        restart-udev = "${systemd}/bin/systemctl restart systemd-udev-trigger.service";
-      };
+        # Fixes https://github.com/NixOS/nixpkgs/issues/195777
+        system.activationScripts = with pkgs; {
+          restart-udev = "${systemd}/bin/systemctl restart systemd-udev-trigger.service";
+        };
 
-    })
-    (mkIf (cfg.enable && cfg.wifi.enable) {
-      networking = {
-        wireless.iwd.enable = true;
-        networkmanager.wifi.backend = "iwd";
-      };
-    })
-  ];
-}
+      })
+      (mkIf (cfg.enable && cfg.wifi.enable) {
+        networking = {
+          wireless.iwd.enable = true;
+          networkmanager.wifi.backend = "iwd";
+        };
+      })
+    ];
+  }
