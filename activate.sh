102c2dc50aaa9f185e515f28bbfb266c434c3a32
Giulio Cocconi <coccogiulio8@gmail.com>
2023-09-06 10:52:50 +0200

    Activation: added update flag

--- activate.sh
+++ activate.sh
@@ -3,4 +3,11 @@
 cd $(dirname $0)
 echo "Building CogiSystems configuration for host $(hostname) :)"
 git pull 2> /dev/null
+
+if [[ $1 == "--upgrade" ]]; then
+	nix flake update
+	git add flake.lock
+	git commit -m "Updated"
+fi
+
 sudo nixos-rebuild switch --impure --flake . --show-trace $@
