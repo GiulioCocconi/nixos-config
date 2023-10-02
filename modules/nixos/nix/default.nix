1ddbd4dbcce6b367763e9aa3e128ad99b4075591
Giulio Cocconi <coccogiulio8@gmail.com>
2023-10-02 16:01:07 +0200

    Added nix-community cache

--- modules/nixos/nix/default.nix
+++ modules/nixos/nix/default.nix
@@ -18,6 +18,15 @@ in
           http-connections = 50;
           warn-dirty = false;
           auto-optimise-store = true;
+
+          substituters = [
+            "https://nix-community.cachix.org"
+          ];
+
+          trusted-public-keys = [
+             "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+          ];
+
         };
 
         gc.automatic = true;
