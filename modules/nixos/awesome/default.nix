261e168e31ff48ecd8f7e280abb0725f3460f658
Giulio Cocconi <coccogiulio8@gmail.com>
2023-11-05 13:14:57 +0100

    Automatic config for awesome

--- modules/nixos/awesome/default.nix
+++ modules/nixos/awesome/default.nix
@@ -5,29 +5,46 @@ with lib.cogisys;
 let
   cfg = config.cogisys.awesome;
   gui = config.cogisys.system.gui;
+
+  luaModules = with pkgs.luajitPackages; [
+    luarocks
+    luautf8
+  ];
+
+  getLuaPath = lib: dir: "${lib}/${dir}/lua/${pkgs.awesome.lua.luaversion}";
+  makeSearchPath = concatMapStrings (path:
+    " --search " + (getLuaPath path "share") +
+    " --search " + (getLuaPath path "lib")
+  );
+  
+
 in
-  {
-    options.cogisys.awesome = with types; {
-      enable = mkBoolOpt false "Enable awesomewm.";
-    };
+{
+  options.cogisys.awesome = with types; {
+    enable = mkBoolOpt false "Enable awesomewm.";
+  };
 
-    config = mkIf cfg.enable {
-      assertions = [(mkAssertionModule gui "GUI" "awesomewm")];
+  config = mkIf cfg.enable {
+    assertions = [(mkAssertionModule gui "GUI" "awesomewm")];
 
-      services.xserver.displayManager.sddm.enable = true;
-      environment.systemPackages = with pkgs; [
-        rofi
-        udiskie
-        picom
-        libnotify
-      ];
+    services.xserver.displayManager.sddm.enable = true;
 
-      services.xserver.windowManager.awesome = {
-        enable = true;
-        luaModules = with pkgs.luajitPackages; [
-          luarocks
-          luautf8
-        ];
-      };
-    };
-  }
+    environment.etc."awesomewm".source = ./config;
+    
+    environment.systemPackages = with pkgs; [
+      rofi
+      udiskie
+      picom
+      libnotify
+      awesome
+    ];
+
+    services.xserver.windowManager.session = singleton
+      { name = "awesome";
+        start =
+          ''
+            ${pkgs.awesome}/bin/awesome ${makeSearchPath luaModules} -c /etc/awesomewm &
+            waitPID=$!
+          '';
+      };  };
+}
