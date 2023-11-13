0d92633b15ec6ee5799278e3714a72b71c8f0e97
Giulio Cocconi <coccogiulio8@gmail.com>
2023-11-13 21:54:02 +0100

    Get awesome config from GH

--- modules/nixos/awesome/default.nix
+++ modules/nixos/awesome/default.nix
@@ -1,33 +1,57 @@
-{ lib, config, options, pkgs, ... }:
+{ lib, config, options, pkgs, inputs, ... }:
 with lib;
 with lib.cogisys;
 
 let
   cfg = config.cogisys.awesome;
   gui = config.cogisys.system.gui;
+
+  getLuaPath = lib: dir: "${lib}/${dir}/lua/${pkgs.awesome.lua.luaversion}";
+  makeSearchPath = lib.concatMapStrings (path:
+    " --search " + (getLuaPath path "share") +
+    " --search " + (getLuaPath path "lib")
+  );
+
+  configPath = inputs.awesome-config.outPath;
+  configFile = "${configPath}/rc.lua";
+
+  configFlags = "--search ${configPath} -c ${configFile}";
+  
+  
+  luaModules = with pkgs.luajitPackages; [
+    luarocks
+    luautf8
+  ];
+  
 in
-  {
-    options.cogisys.awesome = with types; {
-      enable = mkBoolOpt false "Enable awesomewm.";
-    };
+{
+  options.cogisys.awesome = with types; {
+    enable = mkBoolOpt false "Enable awesomewm.";
+  };
 
-    config = mkIf cfg.enable {
-      assertions = [(mkAssertionModule gui "GUI" "awesomewm")];
+  config = mkIf cfg.enable {
+    assertions = [
+      (mkAssertionModule gui "GUI" "awesomewm")
+      (mkAssertion (builtins.pathExists configFile)
+        "Awesome config file (${configFile}) does not exists")
+    ];
 
-      services.xserver.displayManager.sddm.enable = true;
-      environment.systemPackages = with pkgs; [
-        rofi
-        udiskie
-        picom
-        libnotify
-      ];
+    services.xserver.displayManager.sddm.enable = true;
+    environment.systemPackages = with pkgs; [
+      rofi
+      udiskie
+      picom
+      libnotify
+      awesome
+    ];
 
-      services.xserver.windowManager.awesome = {
-        enable = true;
-        luaModules = with pkgs.luajitPackages; [
-          luarocks
-          luautf8
-        ];
+    services.xserver.windowManager.session = singleton
+      { name = "awesome";
+        start =
+          ''
+            ${pkgs.awesome}/bin/awesome ${makeSearchPath luaModules} ${configFlags} &
+            waitPID=$!
+          '';
       };
-    };
-  }
+  };
+}
