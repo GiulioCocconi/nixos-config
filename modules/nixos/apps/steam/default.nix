eac8b12431ca6a9963ad6aa6cfc70ec50e918e02
Giulio Cocconi <coccogiulio8@gmail.com>
2023-11-16 19:05:05 +0100

    Updated

---
+++ modules/nixos/apps/steam/default.nix
@@ -0,0 +1,25 @@
+{ lib, options, config, ...}:
+with lib;
+with lib.cogisys;
+
+let
+  cfg = config.cogisys.apps.steam;
+  gui = config.cogisys.system.gui;
+in
+{
+  options.cogisys.apps.steam = with types; {
+    enable = mkEnableOption "steam";
+  };
+
+  config = mkIf cfg.enable {
+    assertions = [(mkAssertionModule gui "GUI" "Steam")];
+
+    programs.steam = {
+      enable = true;
+      remotePlay.openFirewall = true; 
+      dedicatedServer.openFirewall = true; 
+    };
+
+    hardware.opengl.driSupport32Bit = true; 
+  };
+}
