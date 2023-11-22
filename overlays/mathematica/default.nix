feb311306bfbe299891f795bf520bde85a8ed31f
Giulio Cocconi <coccogiulio8@gmail.com>
2023-11-22 15:30:32 +0100

    Fixed message

--- overlays/mathematica/default.nix
+++ overlays/mathematica/default.nix
@@ -8,8 +8,8 @@ final: prev: {
       hashMode = "recursive";
       message = ''
         The installer is not present! To install matematica, first download the installer from
-        `https://wolfram.com/download-center/mathematica/`.
-        Then run `nix-store --query --hash $(nix store add-path Mathematica_XX.X.X_BNDL_LINUX.sh --name 'Mathematica.sh')` in order to make it available and find the hash.
+        https://wolfram.com/download-center/mathematica/.
+        Then run "nix-store --query --hash \$(nix store add-path Mathematica_XX.X.X_BNDL_LINUX.sh --name 'Mathematica.sh')" in order to make it available and find the hash.
         
       '';
     };
