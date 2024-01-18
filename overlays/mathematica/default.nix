c8f59403de5fe80dea70c8373164fb407ff0decd
Giulio Cocconi <coccogiulio8@gmail.com>
2024-01-18 19:29:14 +0100

    Updated

--- overlays/mathematica/default.nix
+++ overlays/mathematica/default.nix
@@ -4,13 +4,13 @@ final: prev: {
   mathematica = prev.mathematica.override {
     source = prev.requireFile {
       name = "Mathematica.sh";
-      sha256 = "1zzwpx12prszwwhc0xx15riid0c6psh63s5nnn6dna9ds6yi0qvg";
+      sha256 = "0rmrspvf1aqgpvl9g53xmibw28f83vzrrhw6dmfm4l7y05cr315y";
       hashMode = "recursive";
       message = ''
         The installer is not present! To install matematica, first download the installer from
         https://wolfram.com/download-center/mathematica/.
         Then run "nix-store --query --hash \$(nix store add-path Mathematica_XX.X.X_BNDL_LINUX.sh --name 'Mathematica.sh')" in order to make it available and find the hash.
-        
+
       '';
     };
   };
