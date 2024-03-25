dcfded6c4e2a4baba8408e111da37bdb7b4ea20e
Giulio Cocconi <coccogiulio8@gmail.com>
2024-03-25 22:39:27 +0100

    Fixed

--- overlays/mathematica/default.nix
+++ overlays/mathematica/default.nix
@@ -1,18 +1,21 @@
-{config, inputs, ...}:
+{nixpkgs-stable, ...}:
 let
-  stable = import inputs.nixpkgs-stable { config = config.nixpkgs.config; };
+  stable = import nixpkgs-stable {
+    config = {
+      allowUnfree = true;
+    };
+  };
 in
 final: prev: {
-  let stableMathematica = inputs.nixpkgs-stable.legacyPackages.${final.system}.mathematica; in
-  mathematica = stableMathematica.override {
+  mathematica = prev.mathematica.override {
     source = prev.requireFile {
       name = "Mathematica.sh";
       sha256 = "0rmrspvf1aqgpvl9g53xmibw28f83vzrrhw6dmfm4l7y05cr315y";
       hashMode = "recursive";
       message = ''
-        The installer is not present! To install matematica, first download the installer from
-        https://wolfram.com/download-center/mathematica/.
-        Then run "nix-store --query --hash \$(nix store add-path Mathematica_XX.X.X_BNDL_LINUX.sh --name 'Mathematica.sh')" in order to make it available and find the hash.
+      The installer is not present! To install matematica, first download the installer from
+      https://wolfram.com/download-center/mathematica/.
+      Then run "nix-store --query --hash \$(nix store add-path Mathematica_XX.X.X_BNDL_LINUX.sh --name 'Mathematica.sh')" in order to make it available and find the hash.
 
       '';
     };
