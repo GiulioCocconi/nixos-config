2d213403be5048bbaf5cf57f6e9e4d960ff3b17d
Giulio Cocconi <coccogiulio8@gmail.com>
2024-03-25 22:39:11 +0100

    Added diff between generations

--- modules/nixos/nix/default.nix
+++ modules/nixos/nix/default.nix
@@ -60,5 +60,13 @@ in
 
       documentation.nixos.enable = false;
       environment.variables.NIXPKGS_ALLOW_UNFREE = "1";
+
+      system.activationScripts.diff = {
+        supportsDryActivation = true;
+        text = ''
+        ${pkgs.nvd}/bin/nvd --nix-bin-dir=${pkgs.nix}/bin diff \
+        /run/current-system "$systemConfig"
+        '';
+      };
     };
   }
