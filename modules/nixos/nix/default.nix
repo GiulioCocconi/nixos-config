23201858ca97f70c933c1507b51174e9fa177c29
Giulio Cocconi <coccogiulio8@gmail.com>
2024-05-06 17:45:04 +0200

    Fixed indentation & bugs

--- modules/nixos/nix/default.nix
+++ modules/nixos/nix/default.nix
@@ -9,67 +9,68 @@ let
   cfg = config.cogisys.nix;
   light = config.cogisys.light;
 in
-  {
-    options.cogisys.nix = with types; {
-      enable = mkBoolOpt false "Enable nix management.";
-    };
+{
+  options.cogisys.nix = with types; {
+    enable = mkBoolOpt false "Enable nix management.";
+  };
 
-    config = mkIf cfg.enable {
-      nix = {
-        settings = {
-          experimental-features = [ "nix-command" "flakes" ];
-          http-connections = 50;
-          warn-dirty = false;
-          auto-optimise-store = true;
+  config = mkIf cfg.enable {
+    nix = {
+		  package = pkgs.nixVersions.latest;
+      settings = {
+        experimental-features = [ "nix-command" "flakes" ];
+        http-connections = 50;
+        warn-dirty = false;
+        auto-optimise-store = true;
 
-          substituters = [
-            "https://nix-community.cachix.org"
-          ];
-
-          trusted-public-keys = [
-             "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-          ];
-
-        };
-
-        gc = {
-          automatic = true;
-          dates = "weekly";
-
-          options = if light.memory
-            then"-d"
-            else "--delete-older-than 30d";
-        };
-
-        # Channels are evil! Use `nixpkgs` from flake inputs
-        channel.enable = false;
-        nixPath = [
-          "nixpkgs=${pkgs.path}"
+        substituters = [
+          "https://nix-community.cachix.org"
         ];
-        settings.nix-path = config.nix.nixPath;
+
+        trusted-public-keys = [
+          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        ];
 
       };
 
-      services.lorri.enable = true;
-      programs.direnv.enable = true;
+      gc = {
+        automatic = true;
+        dates = "weekly";
 
-      environment.systemPackages = with pkgs; [
-        snowfallorg.flake
-        nixbang
-        nix-melt
-        vulnix
-        nix-output-monitor
+        options = if light.memory
+                  then"-d"
+                  else "--delete-older-than 30d";
+      };
+
+      # Channels are evil! Use `nixpkgs` from flake inputs
+      channel.enable = false;
+      nixPath = [
+        "nixpkgs=${pkgs.path}"
       ];
+      settings.nix-path = config.nix.nixPath;
 
-      documentation.nixos.enable = false;
-      environment.variables.NIXPKGS_ALLOW_UNFREE = "1";
+    };
 
-      system.activationScripts.diff = {
-        supportsDryActivation = true;
-        text = ''
+    services.lorri.enable = true;
+    programs.direnv.enable = true;
+
+    environment.systemPackages = with pkgs; [
+      snowfallorg.flake
+      nixbang
+      nix-melt
+      vulnix
+      nix-output-monitor
+    ];
+
+    documentation.nixos.enable = false;
+    environment.variables.NIXPKGS_ALLOW_UNFREE = "1";
+
+    system.activationScripts.diff = {
+      supportsDryActivation = true;
+      text = ''
         ${pkgs.nvd}/bin/nvd --nix-bin-dir=${pkgs.nix}/bin diff \
         /run/current-system "$systemConfig"
         '';
-      };
     };
-  }
+  };
+}
