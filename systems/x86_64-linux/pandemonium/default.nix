cdcd1860f4b2af3292b27a654889bde5e5483f04
Giulio Cocconi <coccogiulio8@gmail.com>
2024-08-19 14:12:15 +0200

    Pandemonium in progress

---
+++ systems/x86_64-linux/pandemonium/default.nix
@@ -0,0 +1,35 @@
+# Copyright (c) 2024 Giulio Cocconi
+# SPDX-License-Identifier: MIT
+
+{ config, lib, pkgs, ... }:
+with lib;
+with lib.cogisys;
+
+{
+
+  imports = [ ./hardware.nix ];
+
+  cogisys = {
+    suites.scientificWriting = enabled;
+    suites.development = enabled;
+
+    system.networking.wifi = enabled;
+
+    apps.emacs = enabled;
+    system.gui = enabled;
+    tools.gnupg = enabled;
+
+  };
+
+  users.users = mkUsers [{
+    userName = "giulio";
+    fullName = "G. Cocconi";
+    isAdmin = true;
+    packages = with pkgs; [ mathematica racket libsForQt5.kdenlive qucs-s ngspice ];
+  }];
+
+  virtualisation.virtualbox.host.enable = true;
+
+  system.stateVersion = "23.05";
+
+}
