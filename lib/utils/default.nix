818a6882336962fdf4ff431f54ada869f2633e12
Giulio Cocconi <coccogiulio8@gmail.com>
2024-08-19 18:53:31 +0200

    Fixed led

---
+++ lib/utils/default.nix
@@ -0,0 +1,17 @@
+# Copyright (c) 2024 Giulio Cocconi
+# SPDX-License-Identifier: MIT
+
+{lib, pkgs, ...}:
+
+with lib; rec {
+  writeFileService = {file, text}: {
+    wantedBy = [ "multi-user.service" ];
+    startLimitBurst = 5;
+    startLimitIntervalSec = 1;
+    serviceConfig = {
+      Type = "oneshot";
+      Restart = "on-failure";
+      ExecStart = "${pkgs.runtimeShell} -c 'echo ${text} > ${file}'";
+    };
+  };
+}
