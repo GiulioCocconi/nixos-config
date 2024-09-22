39e5e7d67ad66a857f756379f416c33cbe9e9929
Giulio Cocconi <coccogiulio8@gmail.com>
2024-09-22 09:53:09 +0200

    Use remote sudo

--- activate.sh
+++ activate.sh
@@ -7,13 +7,13 @@ echo "Building CogiSystems configuration for host $(hostname) :)"
 if [[ $1 == "--upgrade" ]]; then
 
     if ! git diff --staged --quiet; then
-		echo "Commit the changes before running an upgrade!"
+	echo "Commit the changes before running an upgrade!"
         exit 1
     fi
 
-	nix flake update
-	git add flake.lock flake.nix
-	git commit -m "Updated"
+    nix flake update
+    git add flake.lock flake.nix
+    git commit -m "Updated"
 fi
 
-sudo nixos-rebuild switch --impure --flake . --show-trace $@
+nixos-rebuild switch --impure --use-remote-sudo --flake . --show-trace $@
