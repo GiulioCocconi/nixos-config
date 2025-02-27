fc87dd2f324780bf03aeca836872cd052deec8a0
Giulio Cocconi <coccogiulio8@gmail.com>
2025-02-27 21:24:58 +0100

    fix

--- modules/nixos/services/tailscale/default.nix
+++ modules/nixos/services/tailscale/default.nix
@@ -9,30 +9,23 @@ let
   cfg = config.cogisys.services.tailscale;
   networking = config.cogisys.system.networking;
 in
-  {
-    options.cogisys.services.tailscale = with types; {
-      enable = mkBoolOpt false "Enable tailscale configuration.";
+{
+  options.cogisys.services.tailscale = with types; {
+    enable = mkBoolOpt false "Enable tailscale configuration.";
+  };
+
+  config = mkIf cfg.enable {
+
+    assertions = [(mkAssertionModule networking "networking" "tailscale")];
+
+    services.tailscale = {
+      enable = true;
     };
 
-    config = mkIf cfg.enable {
-
-      assertions = [(mkAssertionModule networking "networking" "tailscale")];
-
-      services.tailscale = {
-        enable = true;
-      };
-
-      networking.firewall = {
-        trustedInterfaces = [ config.services.tailscale.interfaceName ];
-        allowedUDPPorts = [ config.services.tailscale.port ];
-        checkReversePath = "loose";
-      };
-
-      system.activationScripts.tailscale-up.text = ''
-        ${pkgs.tailscale}/bin/tailscale down
-        ${pkgs.tailscale}/bin/tailscale set --posture-checking=true
-        ${pkgs.tailscale}/bin/tailscale up
-      '';
-
+    networking.firewall = {
+      trustedInterfaces = [ config.services.tailscale.interfaceName ];
+      allowedUDPPorts = [ config.services.tailscale.port ];
+      checkReversePath = "loose";
     };
-  }
+  };
+}
